92fbb59ae0c1f9f3701182bd5ee8b39feb1fdac4
Aniruddh Gopale <anigopale@gmail.com>
2017-10-21 00:12:45 +0530

    update signup form, use redux form

--- src/components/signup.js
+++ src/components/signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Header, Form, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { browserHistory } from 'react-router';
+import { Field, reduxForm } from 'redux-form';
 
 class Signup extends Component {
 
@@ -16,26 +17,68 @@ class Signup extends Component {
       }
     }
 
+  renderField(field) {
+    return (
+      <div>
+        <label>{field.label}</label>
+        <input
+        {...field.input}
+        type={field.type}
+        placeholder={field.placeholder}
+        />
+      </div>
+    )
+  }
+
   render() {
     return(
       <div>
         <Header as="h3">Enter Details</Header>
         <Form>
           <Form.Field>
-            <label>User Name:</label>
-            <input placeholder='user name' />
+            <Field
+              label="Username:"
+              type="text"
+              name="username"
+              placeholder="enter username"
+              component={this.renderField}
+            />
           </Form.Field>
           <Form.Field>
-            <label>Email:</label>
-            <input placeholder='email' />
+          <Field
+            label="Email:"
+            type="text"
+            name="username"
+            placeholder="enter email"
+            component={this.renderField}
+          />
           </Form.Field>
           <Form.Field>
-            <label>Password:</label>
-            <input type="password" placeholder='password' />
+          <Field
+            label="Confirm Email:"
+            type="text"
+            name="email2"
+            placeholder="confirm email"
+            component={this.renderField}
+          />
           </Form.Field>
           <Form.Field>
-            <label>Confirm Password:</label>
-            <input type="password" placeholder='password' />
+          <Field
+            label="Password:"
+            type="password"
+            name="password"
+            placeholder="enter password"
+            component={this.renderField}
+          />
+          </Form.Field>
+          <Form.Field>
+          <Field
+            label="Confirm Password:"
+            type="password"
+            name="password2"
+            placeholder="confirm password"
+            component={this.renderField}
+          />
           </Form.Field>
 
           <Button type='submit'>Sign up</Button>
@@ -49,4 +92,7 @@ function mapStateToProps(state) {
   return { auth: state.auth };
 }
 
-export default connect(mapStateToProps)(Signup);
+export default reduxForm({
+  form:'SignupForm'
+})
+(connect(mapStateToProps)(Signup));
